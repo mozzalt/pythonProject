9610967f722ded4949564c7b99d582dfa6306c6b
Jongjin Jung <mozzalt@gmail.com>
2020-12-11 19:50:13 +0900

    Try QT.Core Add

---
+++ main.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
+#------------------------------------------------------------------------------
+# Title     : CCTV image processing SW - JJJ 따라하기...
+# Target OS : Windows, Mac, Linux
+# Author    : JPark, 2020 @ Korea Electronics Technology Institute
+# History   : ver.202009
+#           : ver.202010
+#           : ver.202011
+#           : ver.202012
+#------------------------------------------------------------------------------
+
+#------------------------------------------------------
+# 의존성 (Dependencies)
+#------------------------------------------------------
+
+
+try:
+    from PyQt5.QtCore import (
+        QFileInfo
+    )
